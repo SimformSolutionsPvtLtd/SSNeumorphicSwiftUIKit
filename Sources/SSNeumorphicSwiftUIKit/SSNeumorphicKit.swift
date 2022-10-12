a60debdc87cf465844a1603ecc8a4be125a2cd44
Parth Dumaswala <parth.du@simformsolutions.com>
2022-10-12 15:18:12 +0530

    Added missing sources

--- Sources/SSNeumorphicSwiftUIKit/SSNeumorphicKit.swift
+++ Sources/SSNeumorphicSwiftUIKit/SSNeumorphicKit.swift
@@ -11,47 +11,110 @@ public struct SSNeumorphicKit {
     public enum ColorSchemeType {
         case auto, light, dark
     }
-
+    
     public static var colorSchemeType : ColorSchemeType = .auto
-
-    #if os(macOS)
+    
+#if os(macOS)
     public typealias ColorType = NSColor
     public static func colorType(red: CGFloat, green: CGFloat, blue: CGFloat) -> ColorType {
         .init(red: red, green: green, blue: blue, alpha: 1.0)
     }
-    #else
+#else
     public typealias ColorType = UIColor
     public static func colorType(red: CGFloat, green: CGFloat, blue: CGFloat) -> ColorType {
         .init(red: red, green: green, blue: blue, alpha: 1.0)
     }
-    #endif
+#endif
     
     public static func color(light: ColorType, dark: ColorType) -> Color {
-        #if os(iOS)
+#if os(iOS)
         switch SSNeumorphicKit.colorSchemeType {
-        case .light:
-            return Color(light)
-        case .dark:
-            return Color(dark)
-        case .auto:
-            return Color(.init { $0.userInterfaceStyle == .light ? light : dark })
+            case .light:
+                return Color(light)
+            case .dark:
+                return Color(dark)
+            case .auto:
+                return Color(.init { $0.userInterfaceStyle == .light ? light : dark })
         }
-        #else
+#else
         switch SSNeumorphicKit.colorSchemeType {
-        case .light:
-            return Color(light)
-        case .dark:
-            return Color(dark)
-        case .auto:
-            return Color(.init(name: nil, dynamicProvider: { (appearance) -> NSColor in
-                return appearance.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua ? dark : light
-            }))
+            case .light:
+                return Color(light)
+            case .dark:
+                return Color(dark)
+            case .auto:
+                return Color(.init(name: nil, dynamicProvider: { (appearance) -> NSColor in
+                    return appearance.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua ? dark : light
+                }))
         }
-        #endif
+#endif
     }
-
+    
 }
 
+struct SSNuemorphicView: View {
+    
+    var fillColor: Color = .white
+    var cornerRadius : CGFloat = 15
+    
+    var body: some View {
+        
+        //Rectangle
+        RoundedRectangle(cornerRadius: cornerRadius).fill(fillColor)
+        
+            .outerShadow()
+            .innerShadow(RoundedRectangle(cornerRadius: cornerRadius))
+    }
+}
 
+struct SSNuemorphicTextField: View {
 
+    var placeholder: String
+    var isSecureField = false
+    var hasTrailingImage = false
+    var imageName: String = ""
+    @State var textFieldContent: String = ""
 
+    var body: some View {
+        
+        HStack {
+            
+            if isSecureField {
+                SecureField(placeholder, text: $textFieldContent).padding()
+                    .font(Font.body.weight(.bold))
+            } else {
+                TextField(placeholder, text: $textFieldContent).padding()
+                    .font(Font.body.weight(.bold))
+            }
+            
+            if hasTrailingImage {
+                Image(systemName: imageName)
+                    .foregroundColor(.black)
+            }
+            
+        }.padding(.trailing)
+            .background(
+                RoundedRectangle(cornerRadius: 30)
+                    .fill(Color.white)
+                    .innerShadow(RoundedRectangle(cornerRadius: 30))
+                    .outerShadow()
+            ).padding()
+    }
+}
+
+struct SSNuemorphicButton: View {
+    
+    var text: String = ""
+    var pressedEffect:ButtonPressedEffect = .none
+    var action: () -> Void
+
+    var body: some View {
+    
+        //Button
+        Button(action: action) {
+            Text(text).foregroundColor(.black)
+                .fontWeight(.bold).frame(width: 300, height: 20)
+        }
+        .buttonStyle(Capsule(), padding: 15, pressedEffect: pressedEffect)
+    }
+}
